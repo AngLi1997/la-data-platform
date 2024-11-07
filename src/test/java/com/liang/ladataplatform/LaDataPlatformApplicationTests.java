91074bb5687bfb0b2b74f40c2addf21a5e024002
liang <liangliangaichirou@gmail.com>
2024-11-07 15:11:51 +0800

    init

---
+++ src/test/java/com/liang/ladataplatform/LaDataPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liang.ladataplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaDataPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
